5a39dbcabe8d0efc1008bcd697f15f1023a9b383
Andrei Korchagin <andrei.korchagin@gmail.com>
2019-02-14 16:58:02 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector(){
+
+}
+function lastImageSelector(){
+
+}
+function ninjaBabySelector(){
+
+}
+function divSelector(){
+  return $('.pics');
+}
+function firstListItem(){
+  return $('ul:first-child(#pic-list)');
+}
