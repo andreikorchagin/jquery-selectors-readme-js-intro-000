238c46126de7056a054f70385f9fd955960f120c
Andrei Korchagin <andrei.korchagin@gmail.com>
2019-02-14 17:00:21 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,7 +3,7 @@ function paragraphSelector(){
   return $('p');
 }
 function lastImageSelector(){
-  return $('img:last')
+  return $('img:last');
 }
 function ninjaBabySelector(){
   return $('#baby-ninja');
